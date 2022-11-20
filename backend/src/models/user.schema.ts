bc57fa27e2401cd53deed1ea77864a209ffb06b1
AzureAD\GiorgiGvimradze <giorgi.gvimradze@solutions2share.net>
2022-11-20 11:54:25 +0400

    mobile set EXPO

--- backend/src/models/user.schema.ts
+++ backend/src/models/user.schema.ts
@@ -4,6 +4,7 @@ import * as bcrypt from 'bcrypt';
 export const UserSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+  created: { type: Date, required: true },
 });
 
 UserSchema.pre('save', async function (next) {
