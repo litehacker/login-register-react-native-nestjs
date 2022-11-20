1c27c1e64f0a418e1d8ebb9cec0658fd1132056c
AzureAD\GiorgiGvimradze <giorgi.gvimradze@solutions2share.net>
2022-11-20 11:38:13 +0400

    backend is set up

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost/TestDatabase', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: undefined,
+    }),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
